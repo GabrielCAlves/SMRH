60882772584fe0841fa8a5c536b3cea02758ca70
Gabriel Correa <gabcralves@gmail.com>
2018-09-20 16:49:03 -0300

    Edit file

--- smrh.py
+++ smrh.py
@@ -4,14 +4,21 @@ import pytesseract
 import time
 import os
 
+# Get absolute path to project folder
 PATH = os.path.dirname(os.path.realpath(__file__))
 
+# Take a picture and save it
 def get_picture():
+    # Get current time
     date = (time.strftime('%y-%b-%d_%H:%M'))
     picture = PATH + '/smrh_app/static/images/' + date + '.png'
 
     camera = PiCamera()
-
     camera.rotation = 180
     camera.capture(picture)
     camera.close()
+
+# Run OCR - Tesseract
+def run_tesseract(picture):
+    # Open image
+    img = Image.open(filename)
--- smrh_app/main.py
+++ smrh_app/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import pygal
 
 smrh = Flask(__name__)
 
 @smrh.route('/')
 def index():
-    return render_template('index.html')
+    grap = pygal.Bar()
+    graph.title = 'Consumo de água'
+    graph.x_labels = ['10:00', '10:05', '10:10']
+    graph.add('Consumo em m³', [100, 150, 170])
+    graph_data = graph.render_data_uri
+    return render_template('index.html', graph_data)
 
 if __name__ == '__main__':
     smrh.run(debug=True, host='0.0.0.0')
