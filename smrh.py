6bb5f0eb43e8dd386f572614d6f08bc11f7fb668
Gabriel Correa <gabcralves@gmail.com>
2018-09-20 16:22:45 -0300

    Edit file

--- smrh.py
+++ smrh.py
@@ -4,5 +4,14 @@ import pytesseract
 import time
 import os
 
+PATH = os.path.dirname(os.path.realpath(__file__))
+
 def get_picture():
     date = (time.strftime('%y-%b-%d_%H:%M'))
+    picture = PATH + '/smrh_app/static/images/' + date + '.png'
+
+    camera = PiCamera()
+
+    camera.rotation = 180
+    camera.capture(picture)
+    camera.close()
