14831a80f6d359df4c5aa0d0e819411bb77093d5
Gabriel Correa <gabcralves@gmail.com>
2018-09-20 16:55:52 -0300

    Edit file

--- smrh_app/main.py
+++ smrh_app/main.py
@@ -10,7 +10,7 @@ def index():
     graph.x_labels = ['10:00', '10:05', '10:10']
     graph.add('Consumo em m³', [100, 150, 170])
     graph_data = graph.render_data_uri
-    return render_template('index.html', graph_data)
+    return render_template('index.html', graph_data=graph_data)
 
 if __name__ == '__main__':
     smrh.run(debug=True, host='0.0.0.0')
