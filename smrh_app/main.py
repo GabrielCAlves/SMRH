a1cc073502153b375cc0e818b3e575572323ec75
Gabriel Correa <gabcralves@gmail.com>
2018-09-20 20:35:25 -0300

    Edit file

--- smrh_app/main.py
+++ smrh_app/main.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from pygal.style import BlueStyle
 import pygal
 
 smrh = Flask(__name__)
 
 @smrh.route('/')
 def index():
-    graph = pygal.Bar()
-    graph.title = 'Consumo de água'
+    return render_template('index.html',)
+
+@smrh.route('/graph')
+def graph1():
+    graph = pygal.Line(x_title='Horário Registrado (h)', y_title='Consumo (m³)', show_legend=False, style=BlueStyle)
+    graph.title = 'SMRH - Consumo de Água'
     graph.x_labels = ['10:00', '10:05', '10:10']
-    graph.add('Consumo em m³', [100, 150, 170])
-    graph_data = graph.render_data_uri
-    return render_template('index.html', graph_data=graph_data)
+    graph.add('Consumo', [100, 150, 170])
+    graph_data = graph.render_data_uri()
+    return render_template('graph1.html', graph_data=graph_data)
 
 if __name__ == '__main__':
     smrh.run(debug=True, host='0.0.0.0')
