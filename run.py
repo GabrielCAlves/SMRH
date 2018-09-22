b28272ca0f60bf86694310c209ac2aafedf32e28
Gabriel Correa <gabcralves@gmail.com>
2018-09-21 21:42:04 -0300

    Edit file

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import smrh
+from smrh_lib.functions import get_picture, run_tesseract
 import time
 import glob
 import os
@@ -8,13 +8,17 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 
 # Get current time
 time = time.strftime('%H:%M')
+date = time.strftime('%y-%b-%d_%H:%M')
+
+# picture saving PATH
+picture = PATH + '/smrh_app/static/images/' + date + '.png'
 
 # Take a picture
-smrh.get_picture()
+get_picture(picture)
 
 # Find last file
 list_of_files = glob.glob(PATH + '/smrh_app/static/images/*.png')
 latest_file = max(list_of_files, key=os.path.getctime)
 
 # Run tesseract
-digit = int(smrh.run_tesseract(latest_file))
+digit = int(run_tesseract(PATH, latest_file))
