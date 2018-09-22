00c489921978638e676ea4189dfb0f238883b2e8
Gabriel Correa <gabcralves@gmail.com>
2018-09-22 12:13:12 -0300

    Remove while loop

--- run.py
+++ run.py
@@ -6,59 +6,57 @@ import os
 # Get absolute path to project folder
 PATH = os.path.dirname(os.path.realpath(__file__))
 
-# Execute until ok
-while True:
-    try:
-        # Get current time and date
-        time = datetime.now().strftime('%H:%M')
-        date = datetime.now().strftime('%y-%b-%d_%H:%M')
+try:
+    # Get current time and date
+    time = datetime.now().strftime('%H:%M')
+    date = datetime.now().strftime('%y-%b-%d_%H:%M')
 
-        # picture saving PATH
-        picture = PATH + '/smrh_app/static/images/' + date + '.png'
+    # picture saving PATH
+    picture = PATH + '/smrh_app/static/images/' + date + '.png'
 
-        # Take a picture
-        get_picture(picture)
+    # Take a picture
+    get_picture(picture)
 
-        # Run tesseract
-        digit = int(run_tesseract(PATH, picture))
+    # Run tesseract
+    digit = int(run_tesseract(PATH, picture))
 
-    except:
-        if os.path.exists(picture):
-            os.remove(picture)
+except:
+    if os.path.exists(picture)
+        os.remove(picture)
 
-    else:
-        filename1 = PATH + '/smrh_app/static/data/times.p'
-        filename2 = PATH + '/smrh_app/static/data/readings.p'
-        filename3 = PATH + '/smrh_app/static/data/last_digit.p'
+else:
+    filename1 = PATH + '/smrh_app/static/data/times.p'
+    filename2 = PATH + '/smrh_app/static/data/readings.p'
+    filename3 = PATH + '/smrh_app/static/data/last_digit.p'
 
-        # Open files
-        with open(filename1, 'rb') as file:
-            times = pickle.load(file)
+    # Open files
+    with open(filename1, 'rb') as file:
+        times = pickle.load(file)
 
-        with open(filename2, 'rb') as file:
-            readings = pickle.load(file)
+    with open(filename2, 'rb') as file:
+        readings = pickle.load(file)
 
-        with open(filename3, 'rb') as file:
-            last_digit = pickle.load(file)
+    with open(filename3, 'rb') as file:
+        last_digit = pickle.load(file)
 
-        # Update values
-        reading = update_values(PATH, digit, last_digit, readings[-1])
+    # Update values
+    reading = update_values(PATH, digit, last_digit, readings[-1])
 
-        # Append lists
-        times.append(time)
-        readings.append(reading)
+    # Append lists
+    times.append(time)
+    readings.append(reading)
 
-        # Write in files
-        with open(filename1, 'wb') as file:
-            pickle.dump(times, file)
-            file.close()
+    # Write in files
+    with open(filename1, 'wb') as file:
+        pickle.dump(times, file)
+        file.close()
 
-        with open(filename2, 'wb') as file:
-            pickle.dump(readings, file)
-            file.close()
+    with open(filename2, 'wb') as file:
+        pickle.dump(readings, file)
+        file.close()
 
-        with open(filename3, 'wb') as file:
-            pickle.dump(digit, file)
-            file.close()
+    with open(filename3, 'wb') as file:
+        pickle.dump(digit, file)
+        file.close()
 
-        break;
+    break;
