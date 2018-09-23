16efcdc0cb3624ada6a522530f7f933a99a5e0fd
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 17:39:12 -0300

    Added x_label rotation

--- server/main.py
+++ server/main.py
@@ -26,9 +26,11 @@ def graph():
 
     graph = pygal.Line(x_title='Horário Registrado', y_title='Consumo Total (m³)', show_legend=False, style=BlueStyle, x_label_rotation=10)
     graph.title = 'SMRH - Consumo Total de Água'
+
     # Show last 10 readings/times
     graph.x_labels = times[-10:]
     graph.add('Consumo', readings[-10:])
+
     graph_data = graph.render_data_uri()
     return render_template('graph.html', graph_data=graph_data)
 
