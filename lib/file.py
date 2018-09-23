cc5442014b1e6729df59e4c79230f8d5aa80de81
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 11:38:41 -0300

    File manipulation module

---
+++ lib/file.py
@@ -0,0 +1,10 @@
+import pickle
+
+def read_it(FILE_PATH):
+    with open(FILE_PATH, 'rb') as temp_file:
+        return pickle.load(temp_file)
+
+def write_it(FILE_PATH, variable):
+    with open(FILE_PATH, 'wb') as temp_file:
+        pickle.dump(variable, temp_file)
+        file.close()
