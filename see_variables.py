e70f19a61ace2e659f5e249c108cb493d81b147f
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 12:39:28 -0300

    Edit file

--- see_variables.py
+++ see_variables.py
@@ -3,7 +3,10 @@ import cnf
 
 times = file.read_it(cnf.TIMES_PATH)
 readings = file.read_it(cnf.READINGS_PATH)
+last_digit = file.read_it(cnf.LAST_DIGIT_PATH)
 
 print(times)
 print(" ")
 print(readings)
+print(" ")
+print(last_digit)
