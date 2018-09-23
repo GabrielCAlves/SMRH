0bed569e91e410ba0d9e913c87e0ad7d55f24724
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 19:31:26 -0300

    Edited path

--- lib/run_tesseract.py
+++ lib/run_tesseract.py
@@ -14,7 +14,7 @@ def run_tesseract(PATH, PICTURE_PATH, COORDINATES, THRESHOLD_VALUE):
     fn = lambda x : 255 if x > THRESHOLD_VALUE else 0
     final_image = cropped.convert('L').point(fn, mode='1')
 
-    final_image.save(PATH + '/server/static/data/images/result.png')
+    final_image.save(PATH + '/server/static/images/result.png')
 
     # Return OCR'd value as an integer
     return int(pytesseract.image_to_string(final_image, lang='analog', config='-psm 10 nobatch digits'))
