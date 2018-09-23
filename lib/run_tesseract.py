3656fee19c32960dd75af06227eab84e77e287a6
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 19:24:41 -0300

    Edit files

--- lib/run_tesseract.py
+++ lib/run_tesseract.py
@@ -3,7 +3,7 @@ import pytesseract
 import pickle
 
 # Run OCR - Tesseract
-def run_tesseract(PICTURE_PATH, COORDINATES, THRESHOLD_VALUE):
+def run_tesseract(PATH, PICTURE_PATH, COORDINATES, THRESHOLD_VALUE):
     # Open image
     img = Image.open(PICTURE_PATH)
 
@@ -13,6 +13,8 @@ def run_tesseract(PICTURE_PATH, COORDINATES, THRESHOLD_VALUE):
     # Apply threshold
     fn = lambda x : 255 if x > THRESHOLD_VALUE else 0
     final_image = cropped.convert('L').point(fn, mode='1')
+
+    final_image.save(PATH + '/server/static/data/images/result.png')
 
     # Return OCR'd value as an integer
     return int(pytesseract.image_to_string(final_image, lang='analog', config='-psm 10 nobatch digits'))
