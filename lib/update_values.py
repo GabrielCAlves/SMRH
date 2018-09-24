d4c57a28eceaa1bdf62f08c01b6df902e1b8f40c
Gabriel Correa <gabcralves@gmail.com>
2018-09-23 21:35:57 -0300

    Added equal numbers case

--- lib/update_values.py
+++ lib/update_values.py
@@ -1,6 +1,9 @@
 # Update values
 def update_values(MULTIPLIER, digit, last_digit, last_reading):
     # Test conditions
+    if last_digit == digit:
+        return 0;
+
     if last_digit == 9:
         if digit == 0:
             return last_reading + MULTIPLIER
