95e8fbb8b8f282c0a2d3928b3de2117c98cdd32b
Gabriel Correa <gabcralves@gmail.com>
2018-09-30 20:20:28 -0300

    edit

--- server/main.py
+++ server/main.py
@@ -53,7 +53,7 @@ def graph():
     graph.y_title = 'Consumo Total em Litros'
     graph.dots_size = 4
     graph.tooltip_border_radius = 10
-    graph.spacing = 20
+    graph.margin = 20
 
     times = times[-10:]
     readings = readings[-10:]
