e4f0eac7995335395c04f600bc8974f0b6248888
Gabriel Correa <gabcralves@gmail.com>
2018-10-01 00:26:07 -0300

    edit

--- server/main.py
+++ server/main.py
@@ -124,7 +124,7 @@ def graph3():
         font_family='sans-serif',
         title_font_size=20,
         label_font_size=14,
-        value_font_size=36,
+        value_font_size=34,
         value_label_font_size=14,
         major_label_font_size=14,
         tooltip_font_size=16,
